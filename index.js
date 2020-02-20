068deb773eee66a24bc675d0ccb07ded3b04088f
unknown <cidluiz@gmail.com>
2020-02-20 20:41:52 -0300

    set server initial conditions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello Wolrd!')
+/*
+*Primary file for the API
+*/
+
+//Dependencies
+const http = require('http');
+
+//Set Server
+const server = http.createServer((req, res) =>{
+    res.end('Hello Wolrd\n');
+});
+
+server.listen(3000, () =>{
+    console.log('Listening on port 3000');
+})
