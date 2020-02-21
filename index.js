207a8f7027a5eab3bd72e1deb7db99dedbb66d10
unknown <cidluiz@gmail.com>
2020-02-20 23:24:07 -0300

    parsing request data part II

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 //Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 //Set Server
 const server = http.createServer((req, res) =>{
@@ -17,14 +18,36 @@ const server = http.createServer((req, res) =>{
     let path = parseUrl.pathname;
     let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    //get the query string as an object
+    //ex: localhost:3000/foo/nigh/?fizz=buzz
+    //query -> { fizz: 'buzz' }
+    let queryStringObject = parseUrl.query;
+
     //get the HTTP method
     let method = req.method.toLocaleLowerCase();
 
-    //send response
-    res.end('Hello Wolrd\n');
+    //get the headers as an object
+    let headers = req.headers
 
-    //log the request path
-    console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
+    //get the payload, if any
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    //on means event emitter (stream of data). The name of the event is data
+    req.on('data', (data) =>{
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () =>{
+        buffer += decoder.end();
+        //send response
+        res.end('Hello Wolrd\n');
+
+        //log the request path
+        console.log(`Request received on path: ${trimmedPath} with method: ${method} with query params:`, queryStringObject);
+        console.log(`These are the requests headers: ${headers}`);
+        });
+        console.log(`This is the payload: ${buffer}`);
+        });
 
 });
 
