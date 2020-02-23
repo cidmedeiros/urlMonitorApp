adf5a8c0ed55d60d02de57d371f4142f82f00952
unknown <cidluiz@gmail.com>
2020-02-23 18:03:09 -0300

    init router and handlers

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const util = require('util')
 
-//Set Server
-const server = http.createServer((req, res) =>{
+//Set Server -> it processes the incoming data embedded in the request object then assigns the associated action to it
+const server = http.createServer((req, res) => {
 
     //get the URl and parse it
-    //true param tells the url module to call the query-stream module
+    //the true argmnt tells the url module to call the query-stream module
     let parseUrl = url.parse(req.url, true);
 
     //get the path: 1 -> untrimmed; 2 -> trimmes the slashes out
@@ -30,7 +30,7 @@ const server = http.createServer((req, res) =>{
     //get the headers as an object
     let headers = req.headers
 
-    //get the payload, if any
+    //get the payload as an object, if any
     let decoder = new StringDecoder('utf-8');
     let buffer = '';
     //on means event emitter (stream of data). The name of the event is data
@@ -39,7 +39,26 @@ const server = http.createServer((req, res) =>{
     });
 
     req.on('end', () =>{
+        //cuts off data stream
         buffer += decoder.end();
+
+        //Verifies the handler this request should go to.
+        //If one is not found, it should go to notFound handler.
+        let chosenhandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+
+        //construct the data object to send to the chosenhandler
+        let data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        }
+
+        //setting up a general router to route the request to the handlers
+        
+
+
         //send response
         res.end('Hello Wolrd\n');
 
@@ -49,6 +68,25 @@ const server = http.createServer((req, res) =>{
     });
 });
 
+//Start the server and have it listen to the available port
 server.listen(3000, () =>{
     console.log('Listening on port 3000');
 })
+
+//Define the handlers
+var handlers = {};
+
+//Define Sample Handler
+handlers.sample = (data, callback) => {
+    callback(406, {'name':'sample handler'});
+};
+
+//Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+}
+
+//Define a request router
+const router = {
+    'sample': handlers.sample
+};
