810230e16c97277dab71b6350d34bb44037ddf0b
unknown <cidluiz@gmail.com>
2020-02-23 19:46:45 -0300

    add checks and exports to config file

--- config.js
+++ config.js
@@ -15,3 +15,12 @@ environments.staging = {
     'port': 5000,
     envName = 'production'
 }
+
+//Determine which environment was passed as a command-line argument
+let currentEnv = (typeof process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLocaleLowerCase() : '';
+
+//Determine which environment to export
+let toExportEnv = (typeof currentEnv) == 'object' ? environments[currentEnv] : environments.staging;
+
+//Export the determined ENV
+module.exports = toExportEnv;
