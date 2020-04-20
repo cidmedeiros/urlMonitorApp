6c358ffdba2cdb8b9bb524eb32fe2771bc88799e
unknown <cidluiz@gmail.com>
2020-04-20 19:30:46 -0300

    fix anonymous function syntax

--- public/app.js
+++ public/app.js
@@ -25,15 +25,21 @@ app.client.request = (headers, path, method, queryStringObject, payload, callbac
     callback = typeof(callback) == 'function' ? callback : false;
 
     //for each query string parameter sent, add it to the path
-    let requestUrl = path+'?';
+    let requestUrl = path;
     let counter = 0;
     for(var queryKey in queryStringObject){
-        counter++;
-        //if at least one query string parameter has already been added, prepend new parameter with ampersand
-        if(counter > 1){
-            requestUrl+='&';
+        if(queryStringObject.hasOwnProperty(queryKey)){
+            if(counter == 0){
+                requestUrl+='?'
+            }
+            counter++;
+            // If at least one query string parameter has already been added, preprend new ones with an ampersand
+            if(counter > 1){
+              requestUrl+='&';
+            }
+            // Add the key and value
+            requestUrl+=queryKey+'='+queryStringObject[queryKey];
         }
-        requestUrl+=queryKey+'='+queryStringObject[queryKey];
     }
 
     //Creating a new XMLHttpRequest
@@ -78,59 +84,58 @@ app.client.request = (headers, path, method, queryStringObject, payload, callbac
 }
 
 // Bind the forms
-app.bindForms = () => {
-    document.querySelector("form").addEventListener("submit", (e) => {
-  
-      // Stop it from submitting
-      e.preventDefault();
-      const formId = this.id;
-      const path = this.action;
-      const method = this.method.toUpperCase();
-  
-      // Hide the error message (if it's currently shown due to a previous error)
-      document.querySelector("#"+formId+".formError").style.display = 'hidden';
-  
-      // Turn the inputs into a payload
-      var payload = {};
-      const elements = this.elements;
-      for(var i = 0; i < elements.length; i++){
+app.bindForms = function(){
+    document.querySelector("form").addEventListener("submit", function(e) {
+        // Stop it from submitting
+        e.preventDefault();
+        var formId = this.id;
+        var path = this.action;
+        var method = this.method.toUpperCase();
+
+        // Hide the error message (if it's currently shown due to a previous error)
+        document.querySelector("#"+formId+" .formError").style.display = 'hidden';
+
+        // Turn the inputs into a payload
+        var payload = {};
+        var elements = this.elements;
+        for(var i = 0; i < elements.length; i++){
         if(elements[i].type !== 'submit'){
-          let valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-          payload[elements[i].name] = valueOfElement;
+            var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+            payload[elements[i].name] = valueOfElement;
         }
-      }
-  
-      // Call the API
-      app.client.request(undefined,path,method,undefined,payload,(statusCode,responsePayload) => {
+        }
+
+        // Call the API
+        app.client.request(undefined,path,method,undefined,payload, (statusCode,responsePayload) => {
         // Display an error on the form if needed
         if(statusCode !== 200){
-  
-          // Try to get the error from the api, or set a default error message
-          var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
-  
-          // Set the formError field with the error text
-          document.querySelector("#"+formId+".formError").innerHTML = error;
-  
-          // Show (unhide) the form error field on the form
-          document.querySelector("#"+formId+".formError").style.display = 'block';
-  
+
+            // Try to get the error from the api, or set a default error message
+            var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+
+            // Set the formError field with the error text
+            document.querySelector("#"+formId+" .formError").innerHTML = error;
+
+            // Show (unhide) the form error field on the form
+            document.querySelector("#"+formId+" .formError").style.display = 'block';
+
         } else {
-          // If successful, send to form response processor
-          app.formResponseProcessor(formId,payload,responsePayload);
+            // If successful, send to form response processor
+            app.formResponseProcessor(formId,payload,responsePayload);
         }
-  
-      });
+
+        });
     });
-  };
-  
-  // Form response processor
-  app.formResponseProcessor = (formId,requestPayload,responsePayload) => {
+};
+    
+// Form response processor
+app.formResponseProcessor = (formId,requestPayload,responsePayload) => {
     var functionToCall = false;
     if(formId == 'accountCreate'){
-      // @TODO Do something here now that the account has been created successfully
-      
+    // @TODO Do something here now that the account has been created successfully
+
     }
-  };
+};
   
   // Init (bootstrapping)
   app.init = function(){
