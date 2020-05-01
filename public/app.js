e6f40306d44e926134c46acf0cce83ffbf0f309a
unknown <cidluiz@gmail.com>
2020-05-01 19:35:13 -0300

    add post checks form ajax handling

--- public/app.js
+++ public/app.js
@@ -139,20 +139,38 @@ app.bindForms = function(){
         if(document.querySelector("#"+formId+" .formSuccess")){
           document.querySelector("#"+formId+" .formSuccess").style.display = 'none';
         }
+
         // Turn the inputs into a payload
         var payload = {};
         var elements = this.elements;
         for(var i = 0; i < elements.length; i++){
           if(elements[i].type !== 'submit'){
-            var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-            if(elements[i].name == '_method'){
+            // Determine class of element and set value accordingly
+            var classOfElement = typeof(elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+            var valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+            var elementIsChecked = elements[i].checked;
+            // Override the method of the form if the input's name is _method
+            var nameOfElement = elements[i].name;
+            if(nameOfElement == '_method'){
               method = valueOfElement;
             } else {
-              payload[elements[i].name] = valueOfElement;
+              // Create an payload field named "method" if the elements name is actually httpmethod
+              if(nameOfElement == 'httpmethod'){
+                nameOfElement = 'method';
+              }
+              // If the element has the class "multiselect" add its value(s) as array elements
+              if(classOfElement.indexOf('multiselect') > -1){
+                if(elementIsChecked){
+                  payload[nameOfElement] = typeof(payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                  payload[nameOfElement].push(valueOfElement);
+                }
+              } else {
+                payload[nameOfElement] = valueOfElement;
+              }
             }
-
           }
         }
+
         // If the method is DELETE, the payload should be a queryStringObject instead
         var queryStringObject = method == 'DELETE' ? payload : {};
 
@@ -160,11 +178,13 @@ app.bindForms = function(){
         app.client.request(undefined,path,method,queryStringObject,payload,function(statusCode,responsePayload){
           // Display an error on the form if needed
           if(statusCode !== 200){
+
             if(statusCode == 403){
               // log the user out
               app.logUserOut();
 
             } else {
+
               // Try to get the error from the api, or set a default error message
               var error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
 
@@ -178,6 +198,7 @@ app.bindForms = function(){
             // If successful, send to form response processor
             app.formResponseProcessor(formId,payload,responsePayload);
           }
+
         });
       });
     }
@@ -212,10 +233,11 @@ app.formResponseProcessor = function(formId,requestPayload,responsePayload){
       }
     });
   }
+
   // If login was successful, set the token in localstorage and redirect the user
   if(formId == 'sessionCreate'){
     app.setSessionToken(responsePayload);
-    window.location = '/checks/all';
+    window.location = 'checks/all';
   }
 
   // If forms saved successfully and they have success messages, show them
@@ -227,9 +249,13 @@ app.formResponseProcessor = function(formId,requestPayload,responsePayload){
   // If the user just deleted their account, redirect them to the account-delete page
   if(formId == 'accountEdit3'){
     app.logUserOut(false);
-    window.location = '/account/deleted';
+    window.location = 'account/deleted';
   }
 
+  // If the user just created a new check successfully, redirect back to the dashboard
+  if(formId == 'checksCreate'){
+    window.location = 'checks/all';
+  }
 };
 
 // Get the session token from localstorage and set it in the app.config object
