f0ad52b19758b2f7afb09825ed00b40284937c7f
unknown <cidluiz@gmail.com>
2020-05-17 17:46:00 -0300

    add accountCreate

--- pizzaApp/libPizza/handlers.js
+++ pizzaApp/libPizza/handlers.js
@@ -73,6 +73,37 @@ handlers.sessionCreate = (data,callback) => {
     }
 };
 
+// Create Account
+handlers.accountCreate = (data,callback) => {
+    // Reject any request that isn't a GET
+    if(data.method == 'get'){
+      // Prepare data for interpolation
+      var templateData = {
+        'head.title' : 'Create an Account',
+        'head.description' : 'Signup is easy and only takes a few seconds.',
+        'body.class' : 'accountCreate'
+      };
+      // Read in a template as a string
+      tools.getTemplate('accountCreate',templateData, (err,str)=> {
+        if(!err && str){
+          // Add the universal header and footer
+          tools.addUniversalTemplates(str,templateData, (err,str) => {
+            if(!err && str){
+              // Return that page as HTML
+              callback(200,str,'html');
+            } else {
+              callback(500,undefined,'html');
+            }
+          });
+        } else {
+          callback(500,undefined,'html');
+        }
+      });
+    } else {
+      callback(405,undefined,'html');
+    }
+};
+
 // Edit Your Account
 handlers.accountEdit = (data,callback) => {
     // Reject any request that isn't a GET
