ee92e5dc52097914480bdcd005f11df809df5758
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2020-10-07 18:11:51 -0300

    add cli starter

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 //Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 //Declare the app
 
@@ -13,8 +14,15 @@ var app = {};
 app.init = () => {
     //Start the server
     server.init();
+
     //Start the workers
     workers.init();
+
+    //Start the CLI, but make sure it starts last so worker can load up all the logs and other related data
+    setTimeout(() => {
+        cli.int();
+    }, 50);
+
 };
 
 //Execute
