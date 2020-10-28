59ce34f1a346ee5446b71a7a6f4f4ee6bf11a5fa
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2020-10-28 17:22:40 -0300

    add functions from tools file

--- test/unit.js
+++ test/unit.js
@@ -4,7 +4,7 @@
  */
 
 // Dependencies
-var helpers = require('./../lib/helpers.js');
+var tools = require('./../lib/tools.js');
 var logs = require('./../lib/logs.js');
 var exampleDebuggingProblem = require('./../lib/exampleDebuggingProblem.js');
 var assert = require('assert');
@@ -14,30 +14,30 @@ var unit = {};
 
 
 // Assert that the getANumber function is returning a number
-unit['helpers.getANumber should return a number'] = function(done){
-  var val = helpers.getANumber();
-  assert.equal(typeof(val), 'number');
+unit['tools.getANumber should return a number'] = function(done){
+  var val = tools.getANumber();
+  assert.strictEqual(typeof(val), 'number');
   done();
 };
 
 // Assert that the getANumber function is returning 1
-unit['helpers.getANumber should return 1'] = function(done){
-  var val = helpers.getANumber();
-  assert.equal(val, 1);
+unit['tools.getANumber should return 1'] = function(done){
+  var val = tools.getANumber();
+  assert.strictEqual(val, 1);
   done();
 };
 
 // Assert that the getANumber function is returning 2
-unit['helpers.getNumberOne should return 2'] = function(done){
-  var val = helpers.getANumber();
-  assert.equal(val, 2);
+unit['tools.getNumberOne should return 2'] = function(done){
+  var val = tools.getANumber();
+  assert.strictEqual(val, 2);
   done();
 };
 
 // Logs.list should callback an array and a false error
 unit['logs.list should callback a false error and an array of log names'] = function(done){
   logs.list(true,function(err,logFileNames){
-      assert.equal(err, false);
+      assert.strictEqual(err, false);
       assert.ok(logFileNames instanceof Array);
       assert.ok(logFileNames.length > 1);
       done();
