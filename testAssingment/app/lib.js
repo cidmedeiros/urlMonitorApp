3aa3d2c86b96a737d053e6edc61add831f7ef285
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2020-10-30 18:12:38 -0300

    add even and odd functions

---
+++ testAssingment/app/lib.js
@@ -0,0 +1,11 @@
+/* All the function to be tested
+ */
+
+var lib = {};
+
+lib.evenNumber = function(val){
+    return val % 2
+};
+
+
+module.exports = lib;
--- testAssingment/test/testRunner.js
+++ testAssingment/test/testRunner.js
@@ -2,7 +2,6 @@
  * Tests
  *
  */
-
 // Dependencies
 var lib = require('../app/lib');
 var assert = require('assert');
@@ -12,22 +11,43 @@ var unit = {};
 
 // Assert that the EvenNumber function is returning a number
 unit['lib.EvenNumber should return a number'] = function(done){
-  var val = lib.EvenNumber();
+  var val = lib.evenNumber(2);
   assert.strictEqual(typeof(val), 'number');
   done();
 };
 
 // Assert that the EvenNumber function is returning an even number
-unit['lib.EvenNumber should return even number'] = function(done){
-  var val = lib.EvenNumber();
+unit['lib.EvenNumber should return an even number'] = function(done){
+  var val = lib.evenNumber(4);
   assert.strictEqual(val % 2, 0);
   done();
 };
 
-// Assert that the EvenNumber function is returning 1
-unit['lib.getNumberOne should return 1'] = function(done){
-  var val = lib.EvenNumber();
+// Assert that the EvenNumber function is returning 0
+unit['lib.EvenNumber should return 0'] = function(done){
+  var val = lib.evenNumber(4);
   assert.strictEqual(val % 2, 1);
+  done();
+};
+
+// Assert that the oddNumber function is returning a number
+unit['lib.oddNumber should return a number'] = function(done){
+  var val = lib.oddNumber();
+  assert.strictEqual(typeof(val), 'number');
+  done();
+};
+
+// Assert that the oddNumber function is returning an odd number
+unit['lib.oddNumber should return an odd number'] = function(done){
+  var val = lib.oddNumber();
+  assert.notStrictEqual(val % 2,0);
+  done();
+};
+
+// Assert that the oddNumber function is not returning 0
+unit['lib.oddNumberOne should return 1'] = function(done){
+  var val = lib.oddNumber();
+  assert.notStrictEqual(val % 2, 0);
   done();
 };
 
